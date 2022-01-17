10aa70b2e6d42cecfadd6f7f380626a87725bc13
Anthony Lukes <anthony.lukes@gmail.com>
2022-01-16 18:38:01 -0600

    separate component

---
+++ src/features/TripRoutes/TripRoutesComponent.js
@@ -0,0 +1,28 @@
+import Select from '../../components/select';
+import classes from './styles.module.css'
+import formatRoutes from "../../utils/formatRoutes";
+import formatDirections from "../../utils/formatDirections";
+
+const TripRoutesComponent = (props) => {
+  
+  const { routesData, handleRoutesChange, selectedRoute, directionsData, handleDirectionChange, selectedDirection, stopsData, areStopsFetching } = props
+  
+  return (<div className={classes.container}>
+    <div className={classes.container__section}>
+      <Select label="Routes" id="routes" onChange={handleRoutesChange} options={formatRoutes(routesData)} value={selectedRoute} placeholder="Select a Route" />
+    </div>
+    {selectedRoute && (
+      <div className={classes.container__section}>
+        <Select label="Direction" id="direction" onChange={handleDirectionChange} options={formatDirections(directionsData)} value={selectedDirection} placeholder="Select a Direction" />
+      </div>
+    )}
+    <div className={classes.container__section}>
+      {stopsData && stopsData.length > 0 ? (<ul>{stopsData.map(stop => (
+        <li key={stop.Value}>{stop.Text}</li>
+      ))}</ul>) : null}
+      {areStopsFetching && <div>Loading Stops...</div>}
+    </div>
+  </div>);
+}
+ 
+export default TripRoutesComponent;
--- src/features/TripRoutes/index.js
+++ src/features/TripRoutes/index.js
@@ -1,29 +1,11 @@
 import useTripRoutes from "./useTripRoutes";
-import Select from '../../components/select';
-import classes from './styles.module.css'
-import formatRoutes from "../../utils/formatRoutes";
-import formatDirections from "../../utils/formatDirections";
+import TripRoutesComponent from "./TripRoutesComponent";
 
 const TripRoutes = () => {
   
   const { routesData, handleRoutesChange, selectedRoute, directionsData, handleDirectionChange, selectedDirection, stopsData, areStopsFetching } = useTripRoutes()
   
-  return (<div className={classes.container}>
-    <div className={classes.container__section}>
-      <Select label="Routes" id="routes" onChange={handleRoutesChange} options={formatRoutes(routesData)} value={selectedRoute} placeholder="Select a Route" />
-    </div>
-    {selectedRoute && (
-      <div className={classes.container__section}>
-        <Select label="Direction" id="direction" onChange={handleDirectionChange} options={formatDirections(directionsData)} value={selectedDirection} placeholder="Select a Direction" />
-      </div>
-    )}
-    <div className={classes.container__section}>
-      {stopsData && stopsData.length > 0 ? (<ul>{stopsData.map(stop => (
-        <li key={stop.Value}>{stop.Text}</li>
-      ))}</ul>) : null}
-      {areStopsFetching && <div>Loading Stops...</div>}
-    </div>
-  </div>);
+  return (<TripRoutesComponent routesData={routesData} handleRoutesChange={handleRoutesChange} selectedRoute={selectedRoute} directionsData={directionsData} handleDirectionChange={handleDirectionChange} selectedDirection={selectedDirection} stopsData={stopsData} areStopsFetching={areStopsFetching} />);
 }
  
 export default TripRoutes;
